84ceeb2a3f43fc61e6da42913cbbee1c57765a19
kuma-coffee <daniel099d@gmail.com>
2023-07-19 14:33:20 +0700

    show form post job and store job data

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ use App\Models\Listing;
 // All listings 
 Route::get('/', [ListingController::class, 'index']);
 
+// Show create form 
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store listing data 
+Route::post('/listings', [ListingController::class, 'store']);
+
 // Single listing 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
