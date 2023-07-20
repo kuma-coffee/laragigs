89e29c7ef893cd406f6692630c1b0b17504062d2
kuma-coffee <daniel099d@gmail.com>
2023-07-20 20:30:10 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    colors: {
+      laravel: "#ef3b2d",
+    },
+  },
+  plugins: [],
+}
